3432a2c06a087f7267f4c68b783629a4d6d6c792
Bernardo Carvalho <bernardo.9632@gmail.com>
2024-05-20 09:26:26 +0200

    created text function (for english and german)

--- queries/cart_items.sql
+++ queries/cart_items.sql
@@ -1,23 +1,23 @@
 select 
-  pk_cart_item
-  , fk_order
-  , ds_material_alias
-  , ds_material
-  , fk_material_alias
-  , fk_material
-  , ds_purchase_price_formula
+  cart_item.pk_cart_item
+  , cart_item.fk_order
+  , cart_item.ds_material_alias
+  , cart_item.ds_material
+  , cart_item.fk_material_alias
+  , cart_item.fk_material
+  , cart_item.ds_purchase_price_formula
   , material.cd_alias_language
-  , ds_sale_price_formula
-  , vl_quantity_purchase
-  , vl_quantity_sell
-  , vl_unit_price_purchase
-  , vl_unit_price_sell
+  , cart_item.ds_sale_price_formula
+  , cart_item.vl_quantity_purchase
+  , cart_item.vl_quantity_sell
+  , cart_item.vl_unit_price_purchase
+  , cart_item.vl_unit_price_sell
   , order.ds_status_label
 from prd.gold.fct_cart_item cart_item
 left join prd.gold.fct_order order
 on cart_item.fk_order = order.pk_order
 left join prd.gold.dim_material material
-on cart_item.fk_material = material.pk_material
+on cart_item.fk_material_alias = material.pk_material_alias
 
 where
   vl_unit_price_purchase > 0 
